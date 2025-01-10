e28a1f5c17a869a0cbe8ddee83c0dc340d68422b
Taner Acar <151092957+taneracar@users.noreply.github.com>
2025-01-11 00:38:42 +0300

    changing about me section

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -11,7 +11,7 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-        Computer science student with 1 years of internship experience seeking a
+        Computer engineer with 1 years of internship experience seeking a
         position with a firm to apply educational experience in programming
         language theory, computational complexity theory, and computer graphics.
         Willing to learn through practical, hands-on projects under expert
